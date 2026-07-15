8c06d9a03c108dd6a490af02255b267bef0ccead
Anastasiia <vakulchykanastasiia@gmail.com>
2026-07-15 17:08:35 +0300

    Add registration tests using POM

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -15,6 +15,7 @@ import { HomePage } from '../pages/HomePage';
 import { LoginPage } from '../pages/LoginPage';
 import { MyAccountPage } from '../pages/MyAccountPage';
 import { TestConfig } from '../test.config';
+import { RandomDataUtil } from '../utils/randomDataGenerator';
 
 let config: TestConfig;
 let homePage: HomePage;
@@ -59,3 +60,32 @@ test('User login test @master @sanity @regression', async ({ page }) => {
     expect(isLoggedIn).toBeTruthy();
 
 })
+
+test('Validate logging into the Application using invalid credentials @master @regression', async ({ page }) => {
+
+    const user = RandomDataUtil.getUser();
+
+    // Navigate to Login page via Home page
+    await homePage.clickMyAccount();
+    await homePage.clickLogin();
+
+    // Enter invalid credentials and log in
+    await loginPage.setEmail(user.email);
+    await loginPage.setPassword(user.password);
+    await loginPage.clickLogin();
+
+    // Verify login failed and user stays on Login page
+    await expect(page).toHaveURL(/route=account\/login/);
+
+    // Verify error message is displayed
+    const errorMessage = await loginPage.getloginErrorMessage();
+
+    expect(errorMessage).toContain(
+        'Warning: No match for E-Mail Address and/or Password.'
+    );
+
+    // Verify user is not logged in
+    const isLoggedIn = await myAccountPage.isMyAccountPageExists();
+    expect(isLoggedIn).toBeFalsy();
+
+});
